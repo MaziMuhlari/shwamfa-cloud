17434087618c9ca1541512c7df3d236b9f2b71cf
Mazi Muhlari <mazi.muhlari@gmail.com>
2019-07-17 03:36:04 +0200

    Updates for Reconnects

--- startup/db.js
+++ startup/db.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 const winston = require('winston');
 
 module.exports = function () {
-    mongoose.connect(config.get('db'))
+    mongoose
+        .connect(config.get('db'), {
+            server: {
+                reconnectTries: Number.MAX_VALUE,
+                reconnectInterval: 1000
+            }
+        })
         .then(() => winston.info('Connected to MongoDB...'));
 }
